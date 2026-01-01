dde7ce3b0a0272ceb434b036453f5890ae6808b6
Andreas Moe <mail@andreasmoe.com>
2026-01-01 19:37:56 +0100

    Can add to caldav inbox

---
+++ caldav_inbox.py
@@ -0,0 +1,36 @@
+import configparser
+from typing import Tuple, Optional
+
+import caldav
+from datetime import datetime, timezone
+import uuid
+
+def load_config() -> Optional[Tuple[str, str, str, str]]:
+    """Reads CalDAV credentials and calendar name from config.ini."""
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    url = config["caldav"]["url"]
+    username = config["caldav"]["username"]
+    password = config["caldav"]["password"]
+    calendar_name = config["caldav"]["calendar_name"]
+    return url, username, password, calendar_name
+
+
+def send_todo(todo_str: str):
+    url, username, password, calendar_name = load_config()
+    with caldav.DAVClient(
+        url=url, username=username, password=password
+    ) as client:
+        principal = client.principal()
+        
+        # Find the specific calendar by its display name
+        target_calendar = None
+        for cal in principal.calendars():
+            if cal.name == calendar_name:
+                target_calendar = cal
+                break
+        
+        if not target_calendar:
+            raise EnvironmentError(f"Error: Calendar '{calendar_name}' not found.")
+
+        target_calendar.add_todo(f"BEGIN:VTODO\nUID:{uuid.uuid4()}@zfs-monitoring\nDTSTAMP:{datetime.now(timezone.utc).strftime("%Y%m%dT%H%M%SZ")}\nSUMMARY:{todo_str}\nEND:VTODO")
